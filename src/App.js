cf53bbb5643276c4152a0e42482d10fe37ff7556
JustCodeandChill <inforreceiver@gmail.com>
2021-01-19 12:15:00 -0600

    Decouple Welcome.js to App,js and Normal.js to test the custom skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/welcome.css";
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Hello World from React boilerplate</h1>
+                <p>I feel out of line</p>
+            </div>
+        );
+    }
+}
+
+export default App;
