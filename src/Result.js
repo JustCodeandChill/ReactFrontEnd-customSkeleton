8558da436c0f8531f9076db4bdbe5696d5b33cd2
JustCodeandChill <inforreceiver@gmail.com>
2021-02-27 19:08:23 -0600

    Adding switch-case, a constant, isAlphabetOnly

--- src/Result.js
+++ src/Result.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React from "react";
 
+const addressProperty = {
+  houseNumber: "houseNumber"
+};
 class Result extends React.Component {
-    // So nha chi nen chua so
-    isEmpty = (property) => {
-        return (property === null || property === "" || typeof property === "undefined");
-    }
-    generate = () => {
-        let str = "";
-        if (!this.isEmpty(this.props.addressInfo.houseNumber)) {
-            str += "Number " + this.props.addressInfo.houseNumber;
-        }
-        return str;
-    }
+  // So nha chi nen chua so
+  isEmpty = (property) => {
+    return (
+      property === null || property === "" || typeof property === "undefined"
+    );
+  };
 
-    render() {
-        console.log('state', this.state);
-        console.log('props', this.props);
-        //let emptyString = "";
-        let generatedString = this.generate()
-        return (
-            <div>
-                {this.props.generate ? generatedString : generatedString}
-                
-            </div>
-        )
+	isAlphabetOnly = (str) => {
+		let patt = /^[A-Z]+$/i
+		return patt.test(str)
+	}
+
+  createString = (property) => {
+    let result = "";
+
+    switch (property) {
+      case addressProperty.houseNumber:
+				let stringToTest = this.props.addressInfo[property]
+
+        if (!this.isEmpty(stringToTest)) {
+					if (this.isAlphabetOnly(stringToTest)) {
+						result += "Number " + stringToTest;
+					} else {
+						result += stringToTest + " number";
+					}
+        }
     }
+    return result;
+  };
+  generate = () => {
+    let str = "";
+    str = this.createString("houseNumber");
+    //if (!this.isEmpty(this.props.addressInfo.houseNumber)) {
+    //    str += "Number " + this.props.addressInfo.houseNumber;
+    //}
+
+    //if (!this.isEmpty(this.props.addressInfo.houseNumber)) {
+    //    str += "Number " + this.props.addressInfo.houseNumber;
+    //}
+    return str;
+  };
+
+  render() {
+    console.log('In Result state', this.state);
+    console.log('In Result props', this.props);
+    //let emptyString = "";
+    let generatedString = this.generate();
+    return <div>{this.props.generate ? generatedString : generatedString}</div>;
+  }
 }
 
 export default Result;
